5cb998c77ce094bb7c338bab1aaaaa026177a895
RebeMV15 <rebe.mv15@gmail.com>
2025-06-10 11:03:21 +0200

    fix: update resource paths and base URL configuration

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/globo-quiz/' : '/',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
